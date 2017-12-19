fb10ea3534d414e9481aba4d1e16cd6f389450bf
KimHe <kingdomhe@163.com>
2017-12-19 12:09:36 +0100

    [misc]: change concentration and productivity units
            1. change conc. unit from mM to mol/m^3
            2. change productivity unit from g/m^3 to mol/m^3/s

--- examples/Forward/getParameters_ternary_case4.m
+++ examples/Forward/getParameters_ternary_case4.m
@@ -1,6 +1,6 @@
 function [opt, interstVelocity, Feed, Desorbent] = getParameters(varargin)
 % =============================================================================
-%   Case 3, a 8-zone eight-column case for ternary separation
+%   Case 4, a 8-zone eight-column case for ternary separation
 %
 % This is the function to input all the necessary data for simulation
 % Returns:
@@ -23,52 +23,53 @@ function [opt, interstVelocity, Feed, Desorbent] = getParameters(varargin)
     opt.ABSTOL          = 1e-10;
     opt.INIT_STEP_SIZE  = 1e-14;
     opt.MAX_STEPS       = 5e6;
+    opt.enableDebug     = true;
 
     % The parameter setting for the SMB
     opt.nInterval       = 5;
-    opt.switch          = 1552/opt.nInterval;
+    opt.switch          = 1552/opt.nInterval; % s
     opt.timePoints      = 1000/opt.nInterval;
     opt.Purity_limit    = [0.50, 0.95, 0.50];
     opt.Penalty_factor  = 10;
 
-    opt.enableDebug = true;
-    opt.nZone       = 8;    % 8-zone for ternary separation
+    % Network configuration
+    opt.nZone       = 8;
     opt.nColumn     = 8;
-    opt.structID    = [1 1 1 1 1 1 1 1];
-    opt.intermediate_feed = 'raffinate';
+    opt.structID    = [1 1 1 1 1 1 1 1]; % 8 columns in eight-zone, 1 in each zone
+    opt.intermediate_feed = 'raffinate'; % the feed 2 stream is the raffinate port of first subunit
 
     % Binding: Linear Binding isotherm
     opt.BindingModel = 'LinearBinding';
     opt.nComponents = 3;
-    opt.KA = [0.26 0.43 0.6]; % [comp_A, comp_B, comp_C], A for raffinate2, B for extract2, C for extract1
+    opt.KA = [0.26, 0.43, 0.6]; % [comp_A, comp_B, comp_C], A for raffinate2, B for extract2, C for extract1
     opt.KD = [1, 1, 1];
     opt.compTargID = [3, 2, 1]; % target components at [Extract1 Extract2 Raffinate] ports
 
     % Transport
-    opt.dispersionColumn          = ones(1, opt.nZone) .* 3.8148e-16; % D_{ax}
-    opt.filmDiffusion             = [5.0e-5, 5.0e-5, 5.0e-5]; % K_f
-    opt.diffusionParticle         = [1.6e4, 1.6e4, 1.6e4];    % D_p
+    opt.dispersionColumn          = ones(1, opt.nZone) .* 3.8148e-16;   % D_{ax} m^2/s
+    opt.filmDiffusion             = [5.0e-5, 5.0e-5, 5.0e-5];           % K_f m/s
+    opt.diffusionParticle         = [1.6e4, 1.6e4, 1.6e4];              % D_p m^2/s
     opt.diffusionParticleSurface  = [0.0, 0.0, 0.0];
 
     % Geometry
     opt.columnLength        = 53.6e-2;        % m
     opt.columnDiameter      = 2.60e-2;        % m
-    opt.particleRadius      = 0.325e-4 /2;    % m
+    opt.particleRadius      = 0.325e-2 /2;    % m
     opt.porosityColumn      = 0.38;
     opt.porosityParticle    = 0.00001;        % e_p very small to ensure e_t = e_c
 
     % Parameter units transformation
     % The flow rate of Zone I was defined as the recycle flow rate
-    crossArea = pi * (opt.columnDiameter/2)^2;   % m^2
-    flowRate.recycle    = 0.1395e-6;      % m^3/s
-    flowRate.desorbent1 = 0.0414e-6;      % m^3/s
-    flowRate.extract1   = 0.0348e-6;      % m^3/s
-    flowRate.feed1      = 0.02e-6  ;      % m^3/s
-    flowRate.raffinate1 = 0.0266e-6;      % m^3/s
-    flowRate.desorbent2 = 0.0414e-6;      % m^3/s
-    flowRate.extract2   = 0.0348e-6;      % m^3/s
-    flowRate.feed2      = flowRate.raffinate1; % m^3/s
-    flowRate.raffinate2 = 0.0332e-6;      % m^3/s
+    crossArea = pi * (opt.columnDiameter/2)^2;  % m^2
+    flowRate.recycle    = 0.1395e-6;            % m^3/s
+    flowRate.desorbent1 = 0.0414e-6;            % m^3/s
+    flowRate.extract1   = 0.0348e-6;            % m^3/s
+    flowRate.feed1      = 0.02e-6  ;            % m^3/s
+    flowRate.raffinate1 = 0.0266e-6;            % m^3/s
+    flowRate.desorbent2 = 0.0414e-6;            % m^3/s
+    flowRate.extract2   = 0.0348e-6;            % m^3/s
+    flowRate.feed2      = flowRate.raffinate1;  % m^3/s
+    flowRate.raffinate2 = 0.0332e-6;            % m^3/s
     opt.flowRate_extract1   = flowRate.extract1;
     opt.flowRate_extract2   = flowRate.extract2;
     opt.flowRate_raffinate2 = flowRate.raffinate2;
@@ -84,16 +85,16 @@ function [opt, interstVelocity, Feed, Desorbent] = getParameters(varargin)
     interstVelocity.feed2     = flowRate.feed2 / (crossArea*opt.porosityColumn);         % m/s
     interstVelocity.raffinate2= flowRate.raffinate2 / (crossArea*opt.porosityColumn);    % m/s
 
-    concentrationFeed 	= [0.5, 0.5, 0.5];   % g/m^3 [concentration_compA, concentration_compB]
-    opt.molMass         = [180.16, 180.16, 180.16];
-    opt.yLim            = max(concentrationFeed ./ opt.molMass);
+    concentrationFeed   = [0.5, 0.5, 0.5];          % g/m^3
+    opt.molMass         = [180.16, 180.16, 180.16]; % g/mol
+    opt.yLim            = max(concentrationFeed ./ opt.molMass); % mol/m^3
 
     % Feed concentration setup
     Feed.time = linspace(0, opt.switch, opt.timePoints);
     Feed.concentration = zeros(length(Feed.time), opt.nComponents);
     Desorbent.concentration = zeros(length(Feed.time), opt.nComponents);
     for i = 1:opt.nComponents
-        Feed.concentration(1:end, i) = concentrationFeed(i) / opt.molMass(i);
+        Feed.concentration(1:end, i) = concentrationFeed(i) / opt.molMass(i); % mol/m^3
     end
 
     if opt.nZone == 8
